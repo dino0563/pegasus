2c35f2b55c0ee324440ff6ffb32f1bf6e209710b
dino0563 <bmp.melvin@gmail.com>
2024-06-28 09:18:18 +0700

    added migration, models, controller for blog and portfolio

---
+++ database/migrations/2024_06_28_020120_create_blog_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul', 250);
+            $table->string('gambar', 250)->nullable();
+            $table->string('penulis', 250);
+            $table->date('tanggal');
+            $table->string('deskripsi', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog');
+    }
+};
+
