378f8fcd6c40a9f99cdaed5741a0fcb6c5d51be9
dino0563 <bmp.melvin@gmail.com>
2024-07-02 06:52:41 +0700

    Fix user button

--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -203,7 +203,7 @@
                         <!-- Search -->
                         <div class="navbar-nav align-items-center">
                             <div class="nav-item navbar-search-wrapper mb-0">
-                                <a class="nav-item nav-link search-toggler px-0" href="javascript:void(0);">
+                                <a class="nav-link search-toggler px-0" href="javascript:void(0);" onclick="toggleSearchBar()">
                                     <i class="bx bx-search bx-sm"></i>
                                     <span class="d-none d-md-inline-block text-muted">Search (Ctrl+/)</span>
                                 </a>
